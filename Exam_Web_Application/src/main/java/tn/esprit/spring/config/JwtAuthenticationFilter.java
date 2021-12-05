ef81f36e02ab73038cc69ed472b4000d297277d6
moneimhamadi <moneimhamadi@yahoo.com>
2021-12-05 18:17:56 +0100

    Spring Security Added

--- Exam_Web_Application/src/main/java/tn/esprit/spring/config/JwtAuthenticationFilter.java
+++ Exam_Web_Application/src/main/java/tn/esprit/spring/config/JwtAuthenticationFilter.java
@@ -8,9 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import tn.esprit.spring.services.UserDetailsServiceImpl;
 
 @Component
@@ -25,6 +31,45 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+		final String   requestTokenHeader=  request.getHeader("Authorization");
+		System.out.println(requestTokenHeader);
+		String username=null;
+		String jwtToken=null;
+		
+		if( requestTokenHeader!=null && requestTokenHeader.startsWith("Bearer")){
+			//YES 
+			jwtToken= requestTokenHeader.substring(7);
+			
+			try{
+			username=this.JwtUtil.extractUsername(jwtToken);
+			}
+			catch (ExpiredJwtException e){
+				e.printStackTrace();
+				System.out.println("Token expired");
+			}
+			catch (Exception e){
+				e.printStackTrace();
+				System.out.println("Error");
+			}
+			}
+		else {
+			//NO
+			System.out.println("Invalid Token ,not start with Bearer String");
+		}
+		
+		
+		//Validated
+		if (username!= null && SecurityContextHolder.getContext().getAuthentication()==null)
+		{
+		final UserDetails userDetails=	this.UserDetailsServiceImpl.loadUserByUsername(username);
+		
+		if (this.JwtUtil.validateToken(jwtToken, userDetails)){
+			//Token iS Valid
+			UsernamePasswordAuthenticationToken UsernamePasswordAuthentication=new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+			UsernamePasswordAuthentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+			SecurityContextHolder.getContext().setAuthentication(UsernamePasswordAuthentication);
+		}
+		}
 		
 	}
 	
